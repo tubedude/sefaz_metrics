c960f82e60eea9c101e92b04f5ce74016641c299
Roberto Trevisan <rtrevisan@gmail.com>
2017-12-05 15:58:37 -0200

    Robot

--- lib/sefaz_metrics_web/endpoint.ex
+++ lib/sefaz_metrics_web/endpoint.ex
@@ -8,8 +8,8 @@ defmodule SefazMetricsWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :sefaz_metrics, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :sefaz_metrics, gzip: true,
+    only: ~w(css fonts images js favicon.ico robots.txt google1b444570b6a4e8b7.html sitemap.xml)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.

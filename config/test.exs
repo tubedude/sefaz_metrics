800c49d684e6132aa47dd514e8dee3eda17c7fd7
Roberto Trevisan <beto@trevisan.me>
2018-09-07 14:07:53 -0700

    Adding missing database key

--- config/test.exs
+++ config/test.exs
@@ -15,4 +15,5 @@ config :sefaz_metrics, SefazMetrics.Repo,
   username: System.get_env("DATA_DB_USER"),
   password: System.get_env("DATA_DB_PASS"),
   hostname: System.get_env("DATA_DB_HOST"),
+  database: "gonano",
   pool: Ecto.Adapters.SQL.Sandbox
